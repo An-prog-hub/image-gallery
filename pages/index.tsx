88a45112f6533eaf08d545e9fffc6c36b2491598
An-prog-hub <ac.ankushchauhan.2006@gmail.com>
2022-06-25 17:06:10 +0530

    title editz

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,6 +30,8 @@ type Image = {
 
 export default function Gallery({ images }: { images: Image[] }) {
   return (
+    <>    
+    <h1 className="font-medium text-3xl font-mono flex justify-center pt-10">Gradient Gallery</h1>
     <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
       <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
         {images.map((image) => (
@@ -38,6 +40,7 @@ export default function Gallery({ images }: { images: Image[] }) {
         ;
       </div>
     </div>
+    </>
   );
 }
 
